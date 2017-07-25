fd9439f89bee9dcdf38277e47089b054023c4b9f
Stephen Mitchell <stephenmitchell@Stephens-MacBook-Pro.local>
2017-07-24 21:57:39 -0400

    added photos

--- app/js/main.js
+++ app/js/main.js
@@ -56,172 +56,34 @@
                 }),
 
             $stateProvider
+                .state('onlinecoaching', {
+                    url: '/onlinecoaching',
+                    views: {
+                        'content': {
+                            templateUrl: '../partials/onlinecoaching.html',
+                            controller: 'TableController',
+                            controllerAs: 'vm',
+                        },
+                        'nav': {
+                            templateUrl: '../partials/nav.html',
+                            controller: 'TableController',
+                            controllerAs: 'vm',
+                        },
+                        'footer': {
+                            templateUrl: '../partials/footer.html',
+                            controller: 'navController',
+                            controllerAs: 'vm',
+                        }
+
+                    }
+                }),
+
+            $stateProvider
                 .state('contact', {
                     url: '/contact',
                     views: {
                         'content': {
                             templateUrl: '../partials/contact.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('resume', {
-                    url: '/resume',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/resume.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('jamsesh', {
-                    url: '/jamsesh',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/jamsesh.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('todo', {
-                    url: '/todo',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/todo.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('shopular', {
-                    url: '/shopular',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/shopular.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('instagram', {
-                    url: '/instagram',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/instagram.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('blog', {
-                    url: '/blog',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/blog.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'nav': {
-                            templateUrl: '../partials/nav.html',
-                            controller: 'TableController',
-                            controllerAs: 'vm',
-                        },
-                        'footer': {
-                            templateUrl: '../partials/footer.html',
-                            controller: 'navController',
-                            controllerAs: 'vm',
-                        }
-
-                    }
-                }),
-
-            $stateProvider
-                .state('marvel', {
-                    url: '/marvelsite',
-                    views: {
-                        'content': {
-                            templateUrl: '../partials/marvel.html',
                             controller: 'TableController',
                             controllerAs: 'vm',
                         },
