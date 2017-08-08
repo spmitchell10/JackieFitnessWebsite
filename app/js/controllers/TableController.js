d12735b1e761f09c03911bbd4b107faa2264411f
Stephen Mitchell <stephenmitchell@stephens-mbp.attlocal.net>
2017-08-08 19:38:45 -0400

    navBar hover over

--- app/js/controllers/TableController.js
+++ app/js/controllers/TableController.js
@@ -46,5 +46,41 @@
                     });
                 });
             })
+
+
+            $(document).scroll(function() {
+                const cutoff = $(window).scrollTop()
+                let $navSection = $('.nav-section')
+
+                $navSection.each(function() {
+                    let $this = $(this)
+
+                    if ($this.offset().top + $this.height() > cutoff) {
+                        $navSection.removeClass('current')
+                        $this.addClass('current')
+
+                        let $currentID = $('.current').attr('id')
+
+                        $('.navMenu.active').removeClass('active')
+                        console.log($currentID)
+                        switch ($currentID) {
+                            case "home":
+                                $('.navMenu.portfolio').addClass('active')
+                                break
+                            case "about":
+                                $('.navMenu.about').addClass('active')
+                                break
+                            case "onlinecoaching":
+                                $('.navMenu.onlinecoaching').addClass('active')
+                                break
+                            case "contact":
+                                $('.navMenu.contact').addClass('active')
+                                break
+                            default:
+                                break
+                        }
+
+                        return false
+                    }})});
         })
     })();
